dbe3e98fb8f93dccb77f5a27a8d8d622107290ba
akula-a <22.akulaa@gmail.com>
2022-03-20 08:53:24 -0400

    registration funtionality works

--- public/main.js
+++ public/main.js
@@ -2,29 +2,12 @@
 /////////////////////////////////
 
 // students list of classes they are currently taking
-let class_list = ['Programming Usable Interfaces', 'Principles of Computing', 'Python Basics Lab', 'IS Consulting Project']
-localStorage.setItem('class_list', class_list)
-
-// adds class to current list
-function register(){
-    console.log(localStorage.getItem('class_name'))
-    // if (localStorage.getItem('class_list').includes(class_name)){
-    //     alert("This class is currently in your schedule.")
-    // } 
-    // else {
-    //     // figure out local storage for next assignment
-    //     // let class_list = localStorage.getItem('class_list')
-    //     // console.log(Array.from(class_list))
-    //     // localStorage.setItem('class_list', Array.from(class_list).push(class_name))
-    //     // console.log(class_name)
-    //     // console.log(localStorage.getItem('class_list'))
-    //     class_list.push(class_name)
-    //     alert("You have successfully registered!")
-    // }
-    class_list = class_list + (localStorage.getItem('class_name'))
-    console.log(class_list)
-    alert("You have successfully registered!")
-    return class_list
+function initializeClassList(){
+    if (JSON.parse(localStorage.getItem('class_list')) == null){
+        var list = []
+        alert("i am here")
+        localStorage.setItem('class_list', JSON.stringify(list))
+    }
 }
 
 // removes class from current list
@@ -35,6 +18,25 @@ function removeClass(class_name){
         alert("You have successfully removed the class")
     }
     alert("This class is not currently in your schedule.")
+}
+
+// adds class to current list
+function register(){
+    alert(localStorage.getItem('class_name'))
+    class_name = localStorage.getItem('class_name')
+    class_list = JSON.parse(localStorage.getItem('class_list'))
+    console.log(class_list)
+
+    // checks if already existent class
+    if (class_list.includes(class_name)){
+        alert("This class is currently in your schedule.")
+    } 
+    // else registers for class
+    else {
+        alert(JSON.stringify(class_list + [class_name]))
+        localStorage.setItem('class_list', JSON.stringify(class_list + [class_name]))
+        alert("You have successfully registered!")
+    }
 }
 
 
@@ -124,8 +126,8 @@ function findClass() {
 
 // loading classes based on if the student is currently taking them
 function showRegisteredClasses(){
-    let class_list = register()
-    console.log(class_list)
+    class_list = JSON.parse(localStorage.getItem('class_list'))
+    alert(JSON.parse(localStorage.getItem('class_list')))
     if (class_list.includes('Programming Usable Interfaces')){
         console.log(document.getElementsByClassName('PUI'))
         Array.from(document.getElementsByClassName('PUI')).forEach(elem => {
